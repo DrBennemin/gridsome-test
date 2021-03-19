069fbd1180d06aa851f63ffaf34982058d46fa6a
Benjamin Krzis <benjamin@elisana.digital>
2021-03-19 21:28:29 +0100

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <Layout>
+
+    Geil
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Tach Welt'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
---
+++ src/pages/Platzhalter.vue
@@ -0,0 +1,18 @@
+<template>
+<layout>
+  <div>
+      platzhalter
+      <img src="@/images/mitarbeiter-platzhalter.png" alt="asdfa">
+  </div>
+</layout>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
